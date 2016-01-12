fd17f767f4ccafb3b0b07d1729d8c6eaf9f51839
Kris Sankaran <sankaran.kris@gmail.com>
2016-01-12 14:16:04 -0800

    add pca

--- vignettes/global_patterns.R
+++ vignettes/global_patterns.R
@@ -33,3 +33,16 @@ gp_dca_mvar <- convert_to_mvar(gp_dca)
 gp_dca_mvar <- supp_annotation(gp_dca_mvar, global_patterns)
 plot_mvar_d3(gp_dca_mvar, c("arrow", "point"), height = 800)
 
+## ---- PCA ----
+gp_pca <- ordi(global_patterns@otu_table, method = "ade4_pca")
+gp_pca_mvar <- supp_annotation(gp_pca, global_patterns, "li", "co")
+plot_mvar_d3(gp_pca_mvar, c("point", "arrow"), height = 500)
+
+## ---- preproces-pca ----
+X <- otu_table(global_patterns)@.Data
+hist(log(1 + X), 100)
+hist(log(X), 100)
+
+gp_pca <- ordi(scale(log(1 + X)), method = "ade4_pca", scannf = F, nf = 10)
+gp_pca_mvar <- supp_annotation(gp_pca, global_patterns, "li", "co")
+plot_mvar_d3(gp_pca_mvar, c("point", "arrow"), height = 500)
